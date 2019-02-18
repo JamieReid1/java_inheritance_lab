7e3341aa64a63bb4565aa4fd16c48363d215ca6e
klkirstyleeskl <k.lees@btinternet.com>
2019-02-18 15:37:06 +0000

    initialises super class

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -1,4 +1,40 @@
 package staff;
 
-public class Employee {
+public abstract class Employee {
+    String name;
+    String nationalInsuranceNumber;
+    double salary;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationalInsuranceNumber() {
+        return nationalInsuranceNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double amount){
+        this.salary += amount;
+    }
+
+    public double payBonus(){
+        return this.salary * 0.01;
+
+    }
+
+
+    public void addBonus(){
+        double bonus = payBonus();
+        System.out.println(bonus);
+//        this.salary + bonus;
+    }
+
+
+
+
 }
